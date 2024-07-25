4283007eeeb8400470269324971ae1e57ea46d22
Yogesh Saini <saini.yogesh2381@gmail.com>
2024-07-25 23:47:56 +0530

    updtttttttttttd

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
 import express from "express";
-import cors from "cors";
+import dotenv from "dotenv"
+import cors from 'cors'
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
-import userRouter from './routes/userRouter.js';
+
+import userRouter from './routes/userRouter.js'
 import jobRouter from './routes/jobRouter.js';
-import applicationRouter from './routes/applicationRouter.js';
-import { dbConnection } from './database/dbConnection.js';
-import { errorMiddleware } from './middleware/error.js';
+import applicationRouter from './routes/applicationRouter.js'
+
+import {dbConnection} from './database/dbConnection.js'
+import {errorMiddleware} from './middleware/error.js'
 
 const app = express();
-
-// Set up CORS
+dotenv.config()
+// , 'http://localhost:5173'
 app.use(cors({
-    origin: ['https://job-seeker-frontend-beta.vercel.app', 'http://localhost:5173'],
+    origin: ['https://job-seeker-frontend-beta.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
-}));
+  }));
+
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/"
-}));
+app.use(express.urlencoded({extended: true}));
+app.use(fileUpload ({
+     useTempFiles: true,
+     tempFileDir :"/tmp/"
+})) 
 
-// Connect to database
+
 dbConnection();
 
-// Register routes
-app.use('/api/v1/user', userRouter);
-app.use('/api/v1/job', jobRouter);
-app.use('/api/v1/application', applicationRouter);
+app.get('/', (req, res) => {
+    res.send('Welcome to the Job Seeker API!');
+});
+app.use('/api/v1/user' , userRouter);
+app.use('/api/v1/application' , applicationRouter);
+app.use("/api/v1/job" , jobRouter);
+app.use(errorMiddleware)
 
-// Error handling middleware
-app.use(errorMiddleware);
+
+ 
+
 
 export default app;
+
