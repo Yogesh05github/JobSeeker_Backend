459669cb6473bdb758d49840a58091a736977a4d
Yogesh Saini <saini.yogesh2381@gmail.com>
2024-07-25 22:31:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv"
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+import userRouter from './routes/userRouter.js'
+import jobRouter from './routes/jobRouter.js';
+import applicationRouter from './routes/applicationRouter.js'
+
+import {dbConnection} from './database/dbConnection.js'
+import {errorMiddleware} from './middleware/error.js'
+
+const app = express();
+dotenv.config()
+
+app.use(cors({
+    origin: ['https://job-zee-frontend.vercel.app' , 'http://localhost:5173' ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }));
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(fileUpload ({
+     useTempFiles: true,
+     tempFileDir :"/tmp/"
+})) 
+
+
+dbConnection();
+
+
+app.use('/api/v1/user' , userRouter);
+app.use('/api/v1/application' , applicationRouter);
+app.use("/api/v1/job" , jobRouter);
+app.use(errorMiddleware)
+
+
+ 
+
+
+export default app;
+
---
+++ utils/jwtToken.js
@@ -0,0 +1,18 @@
+export const sendToken = (user , statusCode ,res , message) =>{
+    const token  = user.getJwtToken()
+    const options = {
+        expires: new Date(Date.now() + (5 * 24 * 60 * 60 * 1000)), // 5 days from now
+        httpOnly: true , 
+        secure: true ,
+        sameSite: 'None',
+        
+    };
+    
+
+    res.status(statusCode).cookie("token" , token , options).json({
+        success : true , 
+        user,
+        message,
+        token
+     })
+}
