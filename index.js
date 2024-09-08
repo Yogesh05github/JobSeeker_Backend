19ef65d1ee4d461c9488ce318fda9eff3052c1e2
Yogesh05github <saini.yogesh2381@gmail.com>
2024-09-08 16:04:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import app from './app.js';
-import cloudinary from 'cloudinary';
-import dotenv from "dotenv"
+import { v2 as cloudinary } from 'cloudinary';
 
+cloudinary.config({
+    cloud_name: process.env.c_name,
+    api_key: process.env.c_api,
+    api_secret: process.env.c_secret,
+});
 
-cloudinary.v2.config({
-
-    cloud_name:process.env.c_name,
-    api_key:process.env.c_api , 
-    api_secret:process.env.c_secret
-
-})
- 
-const port  = process.env.PORT||8000;
-app.listen(port , ()=> {
-      console.log(`Server listening on port ${port}`);
-})
+const port = process.env.PORT || 8000;
+app.listen(port, (err) => {
+    if (err) {
+        console.error(`Error starting server: ${err}`);
+    } else {
+        console.log(`Server listening on port ${port}`);
+    }
+});
